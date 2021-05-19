dba38f85cc8aa3dc6cbc507df50cd27a86604760
Diana Castro <dianae.castro@gmail.com>
2021-05-18 17:56:06 -0700

    Almacenamiento firebase

--- app/src/main/java/castro/diana/mydigimind/ui/home/HomeFragment.kt
+++ app/src/main/java/castro/diana/mydigimind/ui/home/HomeFragment.kt
@@ -11,6 +11,9 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import castro.diana.mydigimind.R
 import castro.diana.mydigimind.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.fragment_home.view.*
 import kotlinx.android.synthetic.main.task_view.view.*
 
@@ -18,6 +21,9 @@ class HomeFragment : Fragment() {
 
     private var adapter: AdapterTasks? = null
     private lateinit var homeViewModel: HomeViewModel
+
+    private lateinit var storage : FirebaseFirestore
+    private lateinit var usuario : FirebaseAuth
 
     companion object{
         var tasks = ArrayList<Task>()
@@ -33,18 +39,31 @@ class HomeFragment : Fragment() {
                 ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
 
+        tasks = ArrayList()
+        storage = FirebaseFirestore.getInstance()
+        usuario = FirebaseAuth.getInstance()
+
         if(first){
             fillTasks()
             first = false
         }
 
+        if(!tasks.isEmpty()){
+            adapter = AdapterTasks(root.context, tasks)
+            root.gridView.adapter = adapter
+        }
+
+
+
+        /*
         adapter = AdapterTasks(root.context, tasks)
         root.gridView.adapter = adapter
-
+        */
         return root
     }
 
     fun fillTasks(){
+        /*
         tasks.add(Task("Practice 1", arrayListOf("Monday", "Sunday"), "17:30"))
         tasks.add(Task("Practice 2", arrayListOf("Friday", "Saturday"), "12:30"))
         tasks.add(Task("Practice 3", arrayListOf("Tuesday"), "20:40"))
@@ -53,6 +72,39 @@ class HomeFragment : Fragment() {
         tasks.add(Task("Practice 6", arrayListOf("Monday"), "7:30"))
         tasks.add(Task("Practice 7", arrayListOf("Sunday"), "11:00"))
         tasks.add(Task("Practice 8", arrayListOf("Saturday"), "14:50"))
+         */
+
+        storage.collection("actividades")
+            .whereEqualTo("email", usuario.currentUser.email)
+            .get()
+            .addOnSuccessListener {
+                it.forEach {
+                    var dias = ArrayList<String>()
+                    if (it.getBoolean("lu") == true){
+                        dias.add("Monday")
+                    }
+                    if (it.getBoolean("ma") == true){
+                        dias.add("Tuesday")
+                    }
+                    if (it.getBoolean("mi") == true){
+                        dias.add("Wednesday")
+                    }
+                    if (it.getBoolean("ju") == true){
+                        dias.add("Thursday")
+                    }
+                    if (it.getBoolean("vi") == true){
+                        dias.add("Friday")
+                    }
+                    if (it.getBoolean("sa") == true){
+                        dias.add("Saturday")
+                    }
+                    if (it.getBoolean("do") == true){
+                        dias.add("Sunday")
+                    }
+                    tasks!!.add(Task(it.getString("actividad")!!, dias, it.getString("tiempo")!!))
+                }
+
+            }
     }
 
 
